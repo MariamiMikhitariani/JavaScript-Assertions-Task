b41a4477dcc988ebc91c0a1001823f82352a13fd
MariamMikhitariani <mmikhitariani@solvd.com>
2023-12-27 14:31:19 +0400

    fix: removed .only

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 suite("Unit Tests", function () {
   suite("Basic Assertions", function () {
     // #1
-    test.only("#isNull, #isNotNull", function () {
+    test("#isNull, #isNotNull", function () {
       assert.isNull(
         null,
         "This is an optional error description - e.g. null is null"
